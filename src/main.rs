dd88761ea1619120cac00e354857006ea0e007b8
Elric Milon <whirm@gmx.com>
2018-04-25 17:36:28 +0200

    Talk to bitcoind

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![feature(rustc_private)]
+
+extern crate exonum_bitcoinrpc;
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use exonum_bitcoinrpc::Client;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Urr!");
+    let client = Client::new(
+        "rpc://127.0.0.1:8332",
+        Some("foo".to_string()),
+        Some("bar".to_string()),
+    );
+
+    let block_count = client.getblockcount().unwrap();
+
+    println!("Block count: {}", block_count);
 }
