03c95e3fd4c79927ff7e547594acf1f3c2440c09
Elric Milon <whirm@gmx.com>
2018-04-26 14:34:09 +0200

    Parse Blocks and extract some TX information

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 #![feature(rustc_private)]
 
+extern crate base58;
+extern crate bitcoin;
 extern crate exonum_bitcoinrpc;
 extern crate rand;
+extern crate serialize;
 
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 use exonum_bitcoinrpc::Client;
+use bitcoin::blockdata::block::Block;
+use bitcoin::network::serialize::{deserialize, serialize};
+use serialize::hex::FromHex;
+use base58::{FromBase58, ToBase58};
 
 fn main() {
     println!("Urr!");
@@ -17,6 +24,54 @@ fn main() {
     );
 
     let block_count = client.getblockcount().unwrap();
-
     println!("Block count: {}", block_count);
+
+    for height in block_count - 1..block_count {
+        println!("Fetching {}", height);
+
+        let block_hash = client.getblockhash(height).unwrap();
+        println!("Block hash: {}", &block_hash);
+        let block = client.getblock(block_hash).unwrap().from_hex().unwrap();
+        println!("LEN {}", block.len());
+
+        // println!("Block: {}", block);
+        let block_bytes = block;
+        let decode: Result<Block, _> = deserialize(&block_bytes);
+        // assert!(decode.is_ok());
+        let real_decode = decode.unwrap();
+        // assert_eq!(real_decode.header.version, 1);
+        println!("Header version {}", real_decode.header.version);
+        println!("Transactions:");
+        for tx in &real_decode.txdata {
+            println!("  TX ID: {}", tx.txid());
+            println!("     version: {}", tx.version);
+            println!("     lock time: {}", tx.lock_time);
+            println!("     Inputs:");
+            for inp in &tx.input {
+                println!("    INPUT prev-hash: {}", inp.prev_hash);
+            }
+            println!("     Outputs:");
+            for out in &tx.output {
+                println!("    OUTPUT val: {}", out.value);
+                println!(
+                    "           is_v0_p2wsh: {}",
+                    out.script_pubkey.is_v0_p2wsh()
+                );
+                println!(
+                    "           to_v0_p2wsh: {}",
+                    out.script_pubkey.to_v0_p2wsh()
+                );
+                println!("           is_p2sh: {}", out.script_pubkey.is_p2sh());
+                println!("           to_p2sh: {}", out.script_pubkey.to_p2sh());
+                println!("           is_p2pkh: {}", out.script_pubkey.is_p2pkh());
+                println!("           HEX: {:X}", out.script_pubkey);
+            }
+            println!("  TX =======");
+        }
+        println!("BLOCK ======");
+        println!(
+            "ADDR {:?}",
+            String::from_utf8_lossy(&"1DYve98WqJQmx6cDdW2EQFpnqfwptjV4e9".from_base58().unwrap())
+        );
+    }
 }
